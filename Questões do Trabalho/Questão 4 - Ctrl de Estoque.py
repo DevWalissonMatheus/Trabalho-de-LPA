8fc60d5ba3f51e455335d2b8b4c9d94b04ba4bb7
DevWalissonMatheus <mwalisom9@gmail.com>
2022-05-19 00:33:01 -0300

    Inicio da função cadastrarPeca

--- Questões do Trabalho/Questão 4 - Ctrl de Estoque.py
+++ Questões do Trabalho/Questão 4 - Ctrl de Estoque.py
@@ -1,4 +1,11 @@
 print('\n')
 print('Bem vindo ao Controle de Estoque da Bicicletaria do Walisson Matheus RU: 3989950')
 def cadastrarPeca():
-    print
+    cadastro = {'Nome':[], 'Fabricante':[], 'Valor':[]}
+    for i in range(3):
+        nome = input('Insira o Nome da Peça: ')
+        fabricante = input('Insira o Fabricante da Peça: ')
+        valor = float(input('Insira o Valor da Peça: '))
+        cadastro['Nome'].append(nome)
+        cadastro['Fabricante'].append(fabricante)
+        cadastro['Valor'].append(valor)
