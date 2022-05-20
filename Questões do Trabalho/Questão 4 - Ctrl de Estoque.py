e3b5ec8ec4338696c45690edc2255b2249a375ee
DevWalissonMatheus <mwalisom9@gmail.com>
2022-05-20 11:45:19 -0300

    Função do Menu Inicial e Cadastrar Peças feitos

--- Questões do Trabalho/Questão 4 - Ctrl de Estoque.py
+++ Questões do Trabalho/Questão 4 - Ctrl de Estoque.py
@@ -1,31 +1,47 @@
 print('\n')
 print('Bem vindo ao Controle de Estoque da Bicicletaria do Walisson Matheus RU: 3989950')
-codigo = 1
-ct_pecas = []
-def cadastrarPeca():
-    codigo + 1
-    print('Você escolheu a opção Cadastar Peças.')
-    print('Codigo da peça: ',format(codigo))
-    for i in range(1):
-        nome = input('Insira o Nome da Peça: ')
-        fabric = input('Insira o Fabricante da Peça: ')
-        vlr = float(input('Insira o Valor da Peça: '))
-        dic_pecas = {'Nome' : nome,
-                    'Fabricante' : fabric,
-                    'Valor' : vlr}
-        ct_pecas.append(dic_pecas.copy())
 
-def consultarPeca():
+def m_Inicial():
     while True:
         try:
-            print('Você escolheu a opção Consultar Peças.')
-            print('Escolha a opção desejada:\n1 - Consultar Todas as Peças\n'
-            '2 - Consultar Peças por Código\n3 - Consultar Peças por Fabricante\n'
-            '4 - Retomar')
-            men_op = int(input('>>'))
-            if men_op == 1:
-                print(ct_pecas)
+            print('Escolha a opção desejada: \n1 - Cadastrar Peças\n'
+            '2 - Consultar Peças\n3 - Remover Peças\n4 - Sair')
+            op_menu = int(input('>> '))
+            if op_menu <= 4 and op_menu >= 1:
                 break
+            else:
+                print('Opção Inválida!')
+                m_Inicial()
         except ValueError:
-            print('Insira um dos códigos acima!')
-            continue
+            print('Valor Inválido!')
+    return op_menu
+
+def cadastrarPeca(Código):
+    while True:
+        try:
+            print('Você escolheu a opção de Cadastrar de Peças')
+            print('Código da Peça ', Código)
+            nome = input('Insira o Nome da Peça: ')
+            fabric = input('Insira o Fabricante da Peça: ')
+            valor = float(input('Insira o Valor da Peça: '))
+            pecas_cads = {'Nome' : nome, 'Fabricante' : fabric, 'Valor' : valor}
+            break
+        except ValueError:
+            print('Dado Inválido!')
+    return pecas_cads
+
+cont_codigo = 1
+list_cads = []
+op_desejada = m_Inicial()
+while op_desejada != 0:
+    if op_desejada == 1:
+        cadPecas = cadastrarPeca(cont_codigo)
+        cont_codigo += 1
+        list_cads.append(cadPecas)
+    elif op_desejada == 2:
+        print('2')
+    elif op_desejada == 3:
+        print('3')
+    elif op_desejada == 4:
+        print('Programa Encerrado!')
+        break
